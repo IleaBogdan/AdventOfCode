3661a03b02003bd8c2e1b1ce7e567e759e267a3f
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-12-05 15:47:55 +0200

    Update dec1.cpp

--- dec1.cpp
+++ dec1.cpp
@@ -2,83 +2,41 @@
 
 using namespace std;
 
-ifstream fin("date.in");
-ofstream fout("date.out");
+const string file="date";
+ifstream fin(file+".in");
+ofstream fout(file+".out");
+#define endl '\n';
 
-int getInt(string s, int &i)
-{
-    int n=0;
-    while (i<s.size() && s[i]>='0' && s[i]<='9'){
-        n*=10;
-        n+=s[i]-'0';
-        ++i;
-    }
-    return n;
-}
 void p1()
 {
-    map<int, set<int>>mp;
-    string s;
-    int sum=0;
-    while (fin>>s){
-        if (s.find('|')!=-1){
-            int i=0, x, y;
-            x=getInt(s, i);
-            ++i;
-            y=getInt(s, i);
-            mp[y].insert(x);
-        } else {
-            vector<int> v;
-            for (int i=0; i<s.size(); ++i){
-                v.push_back(getInt(s, i));
-            }
-            bool ok=true;
-            for (int i=0; i<v.size() && ok; ++i){
-                for (int j=i+1; j<v.size() && ok; ++j){
-                    ok=!mp[v[i]].count(v[j]);
-                }
-            }
-            if (ok){
-                sum+=v[v.size()/2];
-            }
-        }
+    int n=1000;
+    int a1[1000], a2[1000];
+    for (int i=0; i<n; ++i){
+        fin>>a1[i]>>a2[i];
+    }
+    sort(a1, a1+n), sort(a2, a2+n);
+    long long sum=0;
+    for (int i=0; i<n; ++i){
+        sum+=abs(a1[i]-a2[i]);
     }
     fout<<sum;
-}
-map<int, set<int>>mp;
-
-bool sor(int a, int b){
-    return !mp[a].count(b);
 }
 void p2()
 {
-    string s;
-    int sum=0;
-    while (fin>>s){
-        if (s.find('|')!=-1){
-            int i=0, x, y;
-            x=getInt(s, i);
-            ++i;
-            y=getInt(s, i);
-            mp[y].insert(x);
-        } else {
-            vector<int> v;
-            for (int i=0; i<s.size(); ++i){
-                v.push_back(getInt(s, i));
-            }
-            bool ok=true;
-            for (int i=0; i<v.size() && ok; ++i){
-                for (int j=i+1; j<v.size() && ok; ++j){
-                    ok=!mp[v[i]].count(v[j]);
-                }
-            } 
-            if (!ok){
-                sort(v.begin(), v.end(), sor);
-                sum+=v[v.size()/2];
-            }
-        }
+    int n=1000;
+    int a1[1000], a2[1000];
+    for (int i=0; i<n; ++i){
+        fin>>a1[i]>>a2[i];
     }
-    fout<<sum;
+    map<int, int>frec;
+    for (int i=0; i<n; ++i){
+        ++frec[a2[i]];
+    }
+    int s=0;
+    for (int i=0; i<n; ++i){
+        s+=(frec[a1[i]]*a1[i]);
+    }
+    fout<<s;
 }
 int main()
 {
