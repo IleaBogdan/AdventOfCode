9e5af2f6e39b64fe8069d8944ddb877049fbbd5a
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-12-06 13:06:54 +0000

    ultima versioune care am gasito functionala pe laptop, pierduta ailalta

--- dec6.cpp
+++ dec6.cpp
@@ -33,7 +33,56 @@ void p1()
     }
     fout<<dist_p;
 }
+vector<string> v;
+bool inMat(int i, int j, int k)
+{
+    return i+di[k]>=0 && 
+            i+di[k]<v.size() &&
+            j+dj[k]>=0 &&
+            j+dj[k]<v[0].size();
+}
+bool stuck(int i, int j)
+{
+    int k=0;
+    int parc[140][140];
+    while (inMat(i, j, k)){
+        ++parc[i][j];;
+        if (parc[i][j]>=5)return true;
+        if (v[i+di[k]][j+dj[k]]=='#'){
+            ++k;
+            k%=4;
+        }
+        i+=di[k], j+=dj[k];
+    }
+    return false;
+}
+void p2()
+{
+    string s;
+    int i=0, j=0, keep_i=0;
+    while (fin>>s){
+        v.push_back(s);
+        if (s.find('^')!=-1){
+            j=s.find('^');
+            keep_i=i;
+        }
+        ++i;
+    }
+    i=keep_i;
+    int cnt=0;
+    for (int ii=0; ii<v.size(); ++ii){
+        for (int jj=0; jj<v[0].size(); ++jj){
+            if (v[ii][jj]!='.'){
+                v[ii][jj]='#';
+                cnt+=stuck(i, j);
+                v[ii][jj]='.';
+            }
+        }
+    }
+    fout<<cnt;
+}
 int main()
 {
-    p1();
+    //p1();
+    p2();
 }
