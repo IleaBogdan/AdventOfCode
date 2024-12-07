ce71454d18f6d7577a1342e0dc542e0042cd0c33
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-12-07 11:37:35 +0200

    dec7 but slow as fuck

--- dec7.cpp
+++ dec7.cpp
@@ -22,14 +22,17 @@ vector<int> v;
 bool valid(int rez, int i, int num=0)
 {
     if (rez==num)return true;
+    if (rez>num)return false;
     if (i<v.size())return valid(rez, i+1, num*v[i]) || valid(rez, i+1, num+v[i]);
     else return false;
 }
 void p1()
 {
     string s;
-    long long tot=0;
+    long long tot=0, line=0;
     while (getline(fin, s)){
+        cout<<line<<endl;
+        ++line;
         int i=0, rez=0;
         for(;s[i]!=':' && i<s.size();++i){
             //cout<<i<<endl;
