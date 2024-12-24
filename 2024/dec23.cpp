035f77888fc486c7f24176cc99eb78d3e7ed3cc8
Batman <139052441+IleaBogdan@users.noreply.github.com>
2024-12-24 15:24:57 +0200

    part 2 in day 23 done

--- 2024/dec23.cpp
+++ 2024/dec23.cpp
@@ -11,7 +11,7 @@ ifstream fin("date.in");
 ofstream fout("date.out");
 
 map<string, set<string>> g;
-void p1()
+void read()
 {
     string s;
     while (fin>>s){
@@ -20,7 +20,11 @@ void p1()
         g[s2].insert(s1);
         //cout<<s1<<" - "<<s2<<endl;
     }
-    s="t";
+}
+void p1()
+{
+    read();
+    string s="t";
     set<string> v;
     for (char c='a'; c<='z'; ++c){
         s.push_back(c);
@@ -42,12 +46,45 @@ void p1()
         }
         s.pop_back();
     }
-    /*for (auto it:v){
-        cout<<it<<endl;
-    }*/
     fout<<v.size();
+}
+set<set<string>> sets;
+set<string> sz;
+int msize=0;
+void connect(string nod, set<string> vec)
+{
+    if (sets.count(vec))return;
+    else sets.insert(vec);
+    if (msize<vec.size()){
+        msize=vec.size();
+        sz=vec;
+    }
+    for (auto it:g[nod]){
+        if (vec.count(it))continue;
+        bool ok=true;
+        for (auto jt:vec){
+            ok=g[it].count(jt);
+            if (!ok)break;
+        }
+        if (ok){
+            vec.insert(it);
+            connect(it, vec);
+            vec.erase(it);
+        }
+    }
+}
+void p2()
+{
+    read();
+    for (auto it:g){
+        connect(it.first, {it.first});
+    }
+    for (auto it:sz){
+        fout<<it<<",";
+    }
 }
 int main()
 {
-    p1();
+    //p1();
+    p2();
 }
