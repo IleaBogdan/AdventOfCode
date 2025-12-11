73d0bc9f075f06fb5b59bd336e42a98c08b81d69
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-12-11 09:00:23 +0200

    dec11 lvl2 done

--- 2025/dec11.cpp
+++ 2025/dec11.cpp
@@ -35,8 +35,63 @@ void part1(void){
     }
     cout<<d["out"]; // sol: 668
 }
+map<string,int>in;
+vector<string>topsort(map<string,vector<string>>&g){
+    vector<string>sol;
+    queue<string>q;
+    q.push("svr");
+    while(!q.empty()){
+        auto i=q.front();
+        q.pop();
+        sol.push_back(i);
+        for(auto it:g[i]){
+            --in[it];
+            if(in[it]==0){
+                q.push(it);
+            }
+        }
+    }
+    return sol;
+}
+void part2(void){
+    string s;
+    map<string,vector<string>>g;
+    while(getline(cin,s)){
+        stringstream sin(s);
+        string x,reader;
+        sin>>x;
+        x.pop_back();
+        while(sin>>reader){
+            in[reader]++;
+            g[x].push_back(reader);
+        }
+    }
+    struct valid{
+        string s;
+        int mask;
+    };
+    queue<valid>q;
+    q.push({"svr",0});
+    size_t sol=0ull;
+    map<string,map<int,size_t>>dp;
+    dp["svr"][0]=1;
+    auto sorted=topsort(g);
+    for(auto node:sorted){
+        for(int mask=0;mask<4;++mask){
+            if(!dp[node][mask])continue;
+            for(auto it:g[node]){
+                auto nmask=mask;
+                if(it=="dac")nmask|=1;
+                if(it=="fft")nmask|=2;
+                dp[it][nmask]+=dp[node][mask];
+            }
+        }
+    }
+    cout<<dp["out"][3];
+}
 int main(...){
     open_file();
-    part1();
+    // part1();
+    part2();
     return 0;
 }
